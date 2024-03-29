13c6b6d184c0e2da2a2bf33b81eddb7ea18f430c
Aakash Das <aakashjsr@gmail.com>
2024-03-30 03:59:50 +0530

    setting value for delete checkbox

--- construction_bids/static/js/site-js.js
+++ construction_bids/static/js/site-js.js
@@ -17,6 +17,18 @@ $(document).ready(function () {
       var newName = $(this).attr("name").replace(reg, prefix);
       $(this).attr({ name: newName, id: newName });
       $(this).val("");
+
+      if ($(this).is(":checkbox")) {
+        $(this).prop("checked", false);
+        $(this).removeAttr("checked"); // Uncheck the checkbox
+      }
+    });
+    newForm.find("label").each(function () {
+      var prefix = modelPrefix + "-" + totalForms + "-";
+      var reg = new RegExp(modelPrefix + "-\\d+-");
+
+      var newName = $(this).attr("for").replace(reg, prefix);
+      $(this).attr({ for: newName });
     });
     newForm.appendTo(formset).removeClass("hidden");
     formset.find("[name=" + totalFormElName + "]").val(totalForms + 1);
@@ -29,4 +41,22 @@ $(document).ready(function () {
   $("#add-client").click(function () {
     addFormset("client-formset", "clients");
   });
+
+  $('#form').submit(function(event) {
+    var formsets = ["task-formset", "client-formset"];
+    formsets.forEach(function(formsetID) {
+      // Iterate over delete checkboxes in the current formset
+      $('#' + formsetID + ' input[type="checkbox"]').each(function () {
+        // If the checkbox is checked, set its value to "on"
+        if ($(this).is(':checked')) {
+          $(this).val('on');
+        } else {
+          // If the checkbox is unchecked, ensure its value is not included in the form data
+          $(this).prop('disabled', true);
+        }
+      });
+    });
+
+  });
+
 });
